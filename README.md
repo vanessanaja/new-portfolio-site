3e2793b8ceb3c13bc9e6d02f2176a6f4a04ef65a
Vanessa Naja <vanessacnaja@gmail.com>
2019-07-25 13:10:04 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Webpack 4 Wordpress Theme Drop-in
+
+This package was created as a drop in for most Wordpress starter themes to integrate Webpack 4 and a few of the useful tools it has to offer. If you need a starter theme, try _s at underscores.me
+
+## Perks
+
+1. Incorporates SCSS with source mapping for easy development.
+2. Incorporates Babel to transpile ES6 to ES5
+3. Incorporates SCSS and JS linting for cleaner development
+4. Keeps custom scss and js separate from starter theme unless you declare otherwise.
+5. Uses browsersync to reload browser on save of php, scss, or js files within your theme. 
+
+## Requirements
+
+Node.js
+
+To Access your dashboard, you will want to use your original local address. 
+
+## Instalation
+
+1. Install your starter theme of choice and activate it.
+
+2. Drop package into your theme folder (Note: there are three invisible files that will not copy over unless you have your invisible files as viewable.)
+
+3. Navigate to your theme folder in terminal
+
+4. Install Node Modules into your theme folder
+
+```
+npm install
+```
+
+5. Update your browsersync settings located at the top of webpack.config.js. The settings assume that your theme name is the same as your project directory name. If it is not, please read the calls to 'dir' and adjust as needed. 
+
+
+6. Add your app.js and app.css to your functions.php file. Note: mytheme should be changed to the name of your theme. It is likely that your theme already contains the outer function code and that you only need to use wp_enqueue_style and wp_enqueue_script.
+
+```
+function mytheme_scripts() {
+  wp_enqueue_style( 'mytheme-custom-styles', get_template_directory_uri() . '/dist/app.css' );
+  wp_enqueue_script( 'mytheme-custom-js', get_template_directory_uri() . '/dist/app.js', array(), '20180624', true );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_scripts' );
+
+```
+
+7. Run NPM
+
+```
+npm run dev
+```
+
+### Build Prod Version
+
+8. To build your production version run the following in terminal
+
+```
+npm run build
+```
+
+## Breakdown
+The working files for css and js are kept in the src folder app.js is the primary js file and app.scss is the primary scss file. Once compiled, they will be placed into a dist folder. 
+
+Should you want your app.css to replace the current theme stylesheet rather than be added as a separate css file, simply create a new scss partial, copy the code from the old style.css file and import it first in your app.scss file. Change the name of app.scss to style.css. Open app.js and modify the import link to style.css. Finally, change the following: ExtractTextPlugin("app.css") to ExtractTextPlugin("../style.css") in your webpack.config.js 
+
+Update your package.json file to reflect your own project. # webpack-wptheme-drop-in
---
+++ webpack.config.js
@@ -0,0 +1,121 @@
+const webpack = require('webpack'),
+      path = require('path'),
+      StyleLintPlugin = require('stylelint-webpack-plugin'),
+      BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+      ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+//BrowserSync Settings 
+//Adjust the dir to equal what ever you have named your theme. 
+//This example uses "theme-folder"
+const dir = 'vanessa-naja';      
+const settings = {
+        // The BrowserSync hostname
+          host: 'localhost',
+          
+          // The port to run BrowserSync's server on
+          port: 3333,
+        
+          // A target to proxy all BrowserSync requests to.
+          // This can be a local web server, Vagrant or a docker container.
+          // This is your local/VM WordPress development site.
+          // This example uses MAMP to serve a wordpress site.
+          proxy: 'localhost:8888/' + dir,
+        
+          // If you have your Site URL for WordPress set to anything else other than the proxy address,
+          // we need to override all URL. In this example I am overriding my site at http://localhost:8888/project-folder
+          urlOverride: 'localhost:8888/' + dir
+};
+
+module.exports = function(env) {
+    return {
+        entry: './src/js/app.js',
+        output: {
+            path: path.resolve( __dirname, 'dist'),
+            filename: "app.js",
+            publicPath: dir + '/wp-content/themes/' + dir + '/dist/'
+        },
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader'
+              }
+            },
+            {
+              enforce: 'pre',
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'eslint-loader',
+              options: {
+                  emitWarning: true,
+              }
+            },
+            // Run Sass through loaders before bundling into `style.css`
+            {
+              test: /\.scss$/,
+              include: [path.resolve(__dirname, 'src', 'scss')],
+              loader: ExtractTextPlugin.extract( [
+                  {
+                      loader: 'css-loader',
+                  },
+                  {
+                      loader: 'sass-loader',
+                      options: {
+                          sourceMap: true
+                      }
+                  }
+              ] )            
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: ''
+                    }
+                  }
+                ]
+              }
+
+          ]
+        },
+        plugins: [
+          new ExtractTextPlugin("app.css"),
+          new StyleLintPlugin({ 
+            syntax: 'scss', 
+            fix: true 
+          }),
+          new BrowserSyncPlugin({
+              host: settings.host,
+              port: settings.port,
+              proxy: settings.proxy,
+              rewriteRules: [
+                  {
+                      match: settings.urlOverride,
+                      fn: function (req, res, match) {
+                          return settings.host + ':' + settings.port;
+                      }
+                  }
+              ],
+              files: [
+                {
+                    match: [
+                        '*.php',
+                        '**/*.php'
+                    ],
+                    fn: function(event, file) {
+                        if (event === "change") {
+                            const bs = require('browser-sync').get('bs-webpack-plugin');
+                            bs.reload();
+                        }
+                    }
+                }
+            ]
+          })
+        ],
+        devtool: 'source-map'
+    }
+}
