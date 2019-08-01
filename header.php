772a86851ee867543a22e809f490b28a6817b44d
Vanessa Naja <vanessacnaja@gmail.com>
2019-08-01 16:06:29 -0700

    style about page background

--- header.php
+++ header.php
@@ -43,7 +43,8 @@
 				?>
 				<p class="site-description"><?php echo $vanessa_naja_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-		</div>.site-branding
+		</div>
+		<!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'vanessa-naja' ); ?></button>
