3873795c7a1cb86f859530abaa686176eed00808
Vanessa Naja <vanessacnaja@gmail.com>
2019-08-08 16:00:05 -0700

    style projects position

--- dist/app.js
+++ dist/app.js
@@ -124,7 +124,7 @@ console.log('JS is being loaded');
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/extract-text-webpack-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\n    }\n    ^\n      Invalid CSS after \"    }\": expected selector or at-rule, was \"}\"\n      in /Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/src/scss/partials/_projects.scss (line 58, column 6)\n    at runLoaders (/Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/webpack/lib/NormalModule.js:313:20)\n    at /Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.render [as callback] (/Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/sass-loader/lib/loader.js:52:13)\n    at Object.done [as callback] (/Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Applications/MAMP/htdocs/vanessa-naja/wp-content/themes/vanessa-naja/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 
