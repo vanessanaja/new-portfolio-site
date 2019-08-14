974f270dd996c50a18473baed88d86b699c7d72d
Vanessa Naja <vanessacnaja@gmail.com>
2019-08-14 16:09:53 -0700

    remove additional footer areas

--- footer.php
+++ footer.php
@@ -36,20 +36,6 @@
 		}
 		?>
 		</div>
-		<div id="footer-sidebar2">
-		<?php
-		if(is_active_sidebar('footer-sidebar-2')){
-		dynamic_sidebar('footer-sidebar-2');
-		}
-		?>
-		</div>
-		<div id="footer-sidebar3">
-		<?php
-		if(is_active_sidebar('footer-sidebar-3')){
-		dynamic_sidebar('footer-sidebar-3');
-		}
-		?>
-		</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
